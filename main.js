b1338c04fd9c3e9cead3c5a35aab9a1c396c5f53
JosanaKla <josana.0205@gmail.com>
2021-10-22 13:23:25 -0300

    integgração para consumir dados da api do github

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function changeSocialMediaLinks() {
     
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`  //Vou pegar o primeiro índice(no caso, o primeiro elemento(0), que é a tag a) dos filhos da li para pegar o href desse primeiro elemento e vou substituir esse href por um em que vai colocando o nome de cada social mídia que guardei na variável social. E, para que apareça o meu user certo de acordo com a respectiva mídia social ao rodar o laço de repetição, eu falo para acessar a variável linksSocialMedia conforme a variável SOCIAL que eu estiver rodando
 
-    alert(li.children[0].href)
   }
   /* document.getElementById('userName').textContent = "Josana Klagenberg 233"   //Vou acessar a DOM, pegar o elemento pelo ID, e o elemento que eu quero pegar se chama userName. Vou usar o .textContent para pegar o conteúdo de dentro desse ID chamado userName */
 
@@ -20,3 +19,19 @@ function changeSocialMediaLinks() {
 }
 
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {   //Criei uma função que vai chamar uma variável que guarda o link com o nome do usuário do github que eu já passei inicialmente
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url).then(response => response.json())   //Essa função vai bater na url da api do github, vai pegar o que ela responde e trazer pra gente. Vamos usar promises ( .then() ) para pegar essa resposta se o fetch der certo, e então, essa resposta fica guardada na variável response. O ocnteúdo dessa variável response vai ser pego e transformado em .json, pois o fetch não sabia que era um json e via a resposta inicialmente apenas como objeto. Quando vamos executar apenas uma coisa com a resposta, não eh necessário colocar o {response.json()} entre chaves.
+  .then(data => {  //então agora vou pegar a resposta que foi transformada em .json, colocar ela numa variável chamada data
+    userName.textContent = data.name   //e vou transformar o nome que está lá no HTML para pegar o nome referente api do github do meu usuário 
+    userBio.textContent = data.bio
+    userGithub.href = data.html_url    //aqui coloco .href pois dá p acessar o href do id userGithub dessa forma direta
+    userImage.src = data.avatar_url
+    userLogin.textContent = data.login
+  })  
+}
+
+getGitHubProfileInfos()
+
